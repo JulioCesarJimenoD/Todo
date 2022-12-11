d467ec4280f16880de20ae7ee9f6d64b2a55f4b7
Julio Cesar Jimeno <Julio_jimeno@ucne.edu.do>
2022-12-10 23:07:02 -0400

    Update2 Api

--- app/src/main/java/ucne/edu/todo/ui/screen/tarea/TareaListState.kt
+++ app/src/main/java/ucne/edu/todo/ui/screen/tarea/TareaListState.kt
@@ -4,6 +4,6 @@ import ucne.edu.todo.data.remote.dto.TareaDto
 
 data class TareaListState(
     val isLoading: Boolean = false,
-    val tarea:  List<TareaDto> = emptyList()
+    val tarea:  List<TareaDto> = emptyList(),
     val error: String = ""
 )
