dba91648e5cb10b2d8b824528d2ed5444cdb0e80
Julio Cesar Jimeno <Julio_jimeno@ucne.edu.do>
2022-12-10 21:19:01 -0400

    Update2 Api

--- app/src/main/java/ucne/edu/todo/di/DataModule.kt
+++ app/src/main/java/ucne/edu/todo/di/DataModule.kt
@@ -2,12 +2,17 @@ package ucne.edu.todo.di
 
 import android.content.Context
 import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import ucne.edu.todo.data.TareaDatabase
+import ucne.edu.todo.data.remote.TareaApi
 import ucne.edu.todo.utils.Constante.DATABASE_NAME
 import javax.inject.Singleton
 
@@ -29,6 +34,26 @@ object DataModule {
     @Singleton
     @Provides
     fun provideDao(database: TareaDatabase) = database.tareaDao()
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesTareaApi(moshi: Moshi): TareaApi {
+        return Retrofit.Builder()
+            .baseUrl("http://www.todoapp.somee.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TareaApi::class.java)
+    }
+
 
 
 }
